5625c9ad30a2c6d0a28d911d2f3b57e3275a9716
Babatunde Daniel <daniel.babatunde@whytelabeltech.com>
2026-09-07 19:32:33 +0100

    improve performance with code changes on CalendarIcon

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedEvents from "@/components/home/FeaturedEvents";
 import HeroSection from "@/components/home/HeroSection";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
